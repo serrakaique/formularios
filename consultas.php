6cbda6a1cdd0ea54513771def3b9a09b932fa9d3
serrakaique <79609744+serrakaique@users.noreply.github.com>
2021-03-28 07:26:58 -0300

    Update consultas.php

--- consultas.php
+++ consultas.php
@@ -37,11 +37,32 @@ $registros = mysqli_num_rows($consulta);
 
                 $codigo = $exibirRegistros[0];
                 $nome = $exibirRegistros[1];
+				$DNascimento = $exibirRegistros[2];
+				$CPF = $exibirRegistros[3];
+				$RG = $exibirRegistros[4];
+				$Telefone = $exibirRegistros[5];
+				$Ende = $exibirRegistros[6];
+				$Ende2 = $exibirRegistros[7];
+				$Ende3 = $exibirRegistros[8];
+				$Ende4 = $exibirRegistros[9];
+				$Ende5 = $exibirRegistros[10];
+				$Ende6 = $exibirRegistros[11];
+				
                 
                 print "<article>";
                 
                 print "$codigo<br>";
                 print "$nome<br>";
+				print "$DNascimento<br>";
+				print "$CPF<br>";
+				print "$RG<br>";
+				print "$Telefone<br>";
+				print "$Ende<br>";
+				print "$Ende2<br>";
+				print "$Ende3<br>";
+				print "$Ende4<br>";
+				print "$Ende5<br>";
+				print "$Ende6";
 
                 print "</article>";
             }
